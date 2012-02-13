fc79bd397baf017427fbf1842b90f716bda17b47
w i l l y <gcomesana@cnio.es>
2012-02-13 19:44:05 +0100

    Delayed...

--- app/util/CustomAjax.js
+++ app/util/CustomAjax.js
@@ -5,15 +5,63 @@
 Ext.define ("TD.util.CustomAjax", {
 	extend: "Ext.data.Connection",
 	singleton: true,
+	requires: ['TD.model.CacheReqParam','TD.model.CacheReq','TD.store.CacheReqs'],
 
 	bodyMasking: false,
 	maskMsg: '',
 
+	requestCache: null, // the cache store
+
+	isRequestFinished: true,
+
+
+	init: function (config) {
+		var myConfig = config || {}
+
+// init the cacheStore
+		requestCache = new TD.store.CacheReqs()
+		if (config.clearCache)
+			requestCache.getProxy().clear()
+		else
+			requestCache.load ()
+
+		requestCache.sync()
+		this.callParent(myConfig)
+	},
+
+
+/**
+ * Override the <i>request</i> method in order to deal with the requests cache
+ * before doing the actual request.
+ * @param opts, the request options as defined in ExtJS 4 API docs
+ */
+	request: function (opts) {
+		var myUrl = opts.url
+		var myParams = opts.params
+
+		var cacheResp = this.fetchRequest (myUrl, myParams)
+		if (!cacheResp)
+			this.callParent(arguments)
+
+		else {
+			var myResp = {}
+			myResp.responseText = cacheResp.jsonresp
+			this.fireEvent('requestcomplete', this, myResp, opts)
+		}
+	},
+
+
+
+	fetchRequest: function (url, params) {
+		return false;
+	},
+	
+
 	listeners: {
 		beforerequest: {
-			fn: function () {
+			fn: function (conn, opts) {
 				if (this.bodyMasking == true) {
-					console.info ("CustomAjax beforerequest: "+this.bodyMasking)
+//					console.info ("CustomAjax beforerequest: "+this.bodyMasking)
 					Ext.getBody().mask(this.maskMsg)
 				}
 				else
@@ -22,9 +70,9 @@ Ext.define ("TD.util.CustomAjax", {
 		},
 
 		requestcomplete: {
-			fn: function () {
+			fn: function (conn, response, opts) {
 				if (this.bodyMasking) {
-					console.info ("CustomAjax requestcomplete")
+//					console.info ("CustomAjax requestcomplete")
 					Ext.getBody().unmask()
 				}
 			}
