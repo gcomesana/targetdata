97210da12e717f1191dae49cd23f552b652da474
w i l l y <gcomesana@cnio.es>
2012-02-13 19:43:23 +0100

    On BDDing CustomAjax...

---
+++ app-test/specs/ajaxcache.js
@@ -0,0 +1,18 @@
+
+describe ("Scenarios and specs for CustomAjax", function () {
+
+	describe ("CustomAjax with empty cache", function () {
+
+		beforeEach (function () {
+// TODO add spies here to see whether or not the methods are called
+			TD.util.CustomAjax.init ()
+
+		})
+	})
+
+
+	describe ("CustomAjax with items in cache", function () {
+
+	})
+
+})
